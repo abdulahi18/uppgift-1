4cd481d5071b20ea57c2fa22501bf0fb5acba42c
abib05001 <abib05001@E-PW0234M2>
2023-09-15 12:25:05 +0200

    Add project files.

---
+++ uppgift 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace uppgift
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"   /\\");
+            Console.WriteLine("\"  /  \\");
+            Console.WriteLine("\" /    \\");
+            Console.WriteLine("\"/      \\");
+        }
+       
+    }
+}
+
